811089734ba254673f8c8fb3a748c7faf14bd90a
vipin-shiprocket <vipin.saini@shiprocket.com>
2023-08-16 15:30:02 +0530

    indexed table view sorting show/hide columns

---
+++ src/app/components/indexed-table/indexed-table.component.ts
@@ -0,0 +1,107 @@
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  IColumnData,
+  IndexedTableModules,
+  LooseObject,
+} from './indexed-table.model';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { SelectionModel } from '@angular/cdk/collections';
+
+@Component({
+  selector: 'app-indexed-table',
+  standalone: true,
+  imports: [CommonModule, ...IndexedTableModules],
+  templateUrl: './indexed-table.component.html',
+  styleUrls: ['./indexed-table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class IndexedTableComponent implements AfterViewInit {
+  @Output() saveColumns = new EventEmitter();
+  @Input() set tableData(value: LooseObject[]) {
+    if (value?.length) {
+      this.dataSource = new MatTableDataSource(value);
+    }
+  }
+  @Input() set displayedColumns(value: IColumnData[]) {
+    if (value?.length) {
+      this._displayedColumns = value;
+      this.columnsToDisplay = this.getColumnArrangement();
+    }
+  }
+
+  @Input() set editMode(value: boolean) {
+    this._enableEditMode = value;
+
+    if (value === true) {
+      // show all columns
+      this.columnsToDisplay = this._displayedColumns.map((d) => d.name).slice();
+    } else {
+      const columnArrangement = this.getColumnArrangement();
+      this.saveColumns.emit(columnArrangement);
+      this.columnsToDisplay = columnArrangement;
+    }
+  }
+
+  @Input() enableSort = false;
+
+  @ViewChild(MatSort) sort!: MatSort;
+  columnsToDisplay: string[] = [];
+  _displayedColumns: IColumnData[] = [];
+  _enableEditMode = false;
+  dataSource: MatTableDataSource<LooseObject> = new MatTableDataSource(
+    undefined,
+  );
+  selection = new SelectionModel<LooseObject>(true, []);
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  toggleAllRows() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+
+    this.selection.select(...this.dataSource.data);
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: never): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
+      row['position'] + 1
+    }`;
+  }
+
+  hideColumn(columnData: IColumnData) {
+    columnData['visible'] = !columnData['visible'];
+  }
+
+  getColumnArrangement() {
+    return this._displayedColumns
+      .filter((d) => d.visible)
+      .map((d) => d.name)
+      .slice();
+  }
+}
---
+++ src/app/components/indexed-table/indexed-table.model.ts
@@ -0,0 +1,22 @@
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+
+export const IndexedTableModules = [
+  MatTableModule,
+  FormsModule,
+  MatIconModule,
+  MatSortModule,
+  MatCheckboxModule,
+];
+
+export interface IColumnData {
+  name: string;
+  canHide: boolean;
+  visible: boolean;
+}
+
+/* eslint-disable-next-line */
+export type LooseObject = Record<string, any>;
