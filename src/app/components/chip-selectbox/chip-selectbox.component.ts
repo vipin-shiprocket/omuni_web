e0a46bdcda07c53f0cf209e0c1cf7488f6dd9f85
vipin-shiprocket <vipin.saini@shiprocket.com>
2023-08-22 16:25:49 +0530

    Added custom multi-select using chip

---
+++ src/app/components/chip-selectbox/chip-selectbox.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ChipSelectBoxModules,
+  IOption,
+  chipSelectboxType,
+} from './chip-selectbox.model';
+import { ListboxValueChangeEvent } from '@angular/cdk/listbox';
+
+@Component({
+  selector: 'app-chip-selectbox',
+  standalone: true,
+  imports: [CommonModule, ChipSelectBoxModules],
+  templateUrl: './chip-selectbox.component.html',
+  styleUrls: ['./chip-selectbox.component.scss'],
+})
+export class ChipSelectboxComponent {
+  @Output() selectionChange = new EventEmitter();
+  @Input() disabled = false;
+  @Input() label = '';
+  @Input() options: IOption[] = [];
+  @Input() type: chipSelectboxType = 'select';
+  @Input() multiple = false;
+  selectedValues: IOption[] = [];
+
+  formatValue(value: IOption[]): string {
+    if (!value.length) return '';
+    return `: ${value.map((v) => v.display).join(',')}`;
+  }
+
+  onSelectionChange(selected: ListboxValueChangeEvent<unknown>) {
+    this.selectionChange.emit(selected.value);
+  }
+}
