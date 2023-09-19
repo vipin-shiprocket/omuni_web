7ef5d91b54589cc9d4ff1f5b6baae0d9e4f76e9a
vipin-shiprocket <vipin.saini@shiprocket.com>
2023-09-19 16:17:25 +0530

    [feat](add single order) ui for package details

--- src/app/pages/add-order/single-order/package-details/package-details.component.ts
+++ src/app/pages/add-order/single-order/package-details/package-details.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, ViewEncapsulation, inject } from '@angular/core';
 import { SingleOrderService } from '../single-order.service';
 
 @Component({
   selector: 'app-package-details',
   templateUrl: './package-details.component.html',
   styleUrls: ['./package-details.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class PackageDetailsComponent {
   soService = inject(SingleOrderService);
--- src/app/pages/add-order/single-order/single-order.service.ts
+++ src/app/pages/add-order/single-order/single-order.service.ts
@@ -6,7 +6,7 @@ import { EnumTabStatus, TabListSingleOrder } from '../add-order.model';
 })
 export class SingleOrderService {
   tabs = TabListSingleOrder;
-  currentTab = 2;
+  currentTab = 3;
 
   onTabChange(context: 'next' | 'prev') {
     if (context === 'next') {
