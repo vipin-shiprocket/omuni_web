67d4f0c449b787baa94499aa2c0ae19684cbf1a6
AnandBFRS01918 <anand.antony@shiprocket.com>
2023-09-23 01:28:50 +0530

    [update] [refactor] - OnlyNumbersDirective decimal logic

--- src/app/utils/only-numbers.directive.ts
+++ src/app/utils/only-numbers.directive.ts
@@ -14,7 +14,24 @@ export class OnlyNumbersDirective {
 
     if (this.noLeadingZeros) initalValue = initalValue.replace(/^0+(?!$)/g, '');
 
-    this.el.nativeElement.value = initalValue.replace(/[^0-9]*/g, '');
+    const decimalPoint = this.el.nativeElement.step
+      ? this.el.nativeElement.step.split('.')[1].length
+      : 0;
+
+    if (decimalPoint) {
+      const val = initalValue.replace(/[^0-9.]*/g, '');
+      const index = val.indexOf('.');
+
+      this.el.nativeElement.value =
+        val.substring(0, index + 1) +
+        val
+          .substring(index + 1)
+          .split('.')
+          .join('')
+          .substring(0, decimalPoint);
+    } else {
+      this.el.nativeElement.value = initalValue.replace(/[^0-9]*/g, '');
+    }
 
     if (initalValue !== this.el.nativeElement.value) {
       event.stopPropagation();
